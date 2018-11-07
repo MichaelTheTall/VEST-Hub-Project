1ce9819c2ca30c480a40535cb2e708f4ee59da67
MichaelTheTall <michaeljhnewman@gmail.com>
2018-11-07 21:28:03 +0000

    CSS redone navbar

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const About = () => (
   <div>
-    <h4>About</h4>
-    <p>Some information about us.</p>
+    <h1>V.E.S.T. HUB DOCK OS</h1>
+    <h3>Vesta Extra Solar Transit Hub Dock Operating System</h3>
+    <h3> © TERRALINK SYSTEMS 2198</h3>
+    <p>ALL RIGHTS RESERVED IN SOL SYSTEM</p>
   </div>
 );
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,36 +5,13 @@ import '../index.css';
 const Navbar = () => (
     <div className="topnav">
       <Link className="navlink" to="/">Home</Link>
-      <Link className="navlink" to="/about">About</Link>
       <Link className="navlink" to="/ships">Ships</Link>
       <Link className="navlink" to="/berths">Berths</Link>
       <Link className="navlink" to="/passengers">Passengers</Link>
       <Link className="navlink" to="/dockOfficers">Dock Officers</Link>
       <Link className="navlink" to="/inspections">Inspections</Link>
-      </div>
-  // <ul>
-  //   <li className="navlink">
-  //     <Link to="/">Home</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/about">About</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/ships">Ships</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/berths">Berths</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/passengers">Passengers</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/dockOfficers">Dock Officers</Link>
-  //   </li>
-  //   <li className="navlink">
-  //     <Link to="/inspections">Inspections</Link>
-  //   </li>
-  // </ul>
-);
+      <Link className="navlink" to="/about">About</Link>
+    </div>
+  );
 
 export default Navbar;
