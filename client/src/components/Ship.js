19e39113cba3ecd90e548d5c69dbb30eee47494c
MichaelTheTall <michaeljhnewman@gmail.com>
2018-11-07 13:46:37 +0000

    tweaked arrivals/departures

--- client/src/components/Ship.js
+++ client/src/components/Ship.js
@@ -4,17 +4,26 @@ import {Link} from 'react-router-dom';
 const Ship = (props) => {
 
   var inspected;
-
-  if(props.ship.inspected){
-    inspected = "Y";
-  } else {
-    inspected = "N";
-  }
+  var berthstatus;
 
   if(!props.ship){
     return null;
   }
 
+  if(props.ship.inspected){
+    inspected = "Inspected";
+  } else {
+    inspected = "Awaiting Inspection";
+  }
+
+  if(props.ship.berth){
+    berthstatus = "In Berth " + props.ship.berth;
+  } else {
+    berthstatus = "Awaiting Arrival";
+  }
+
+
+  
   return (
     <React.Fragment>
         <Link to = {"/ships/" + props.ship.index} className="name">
@@ -22,7 +31,7 @@ const Ship = (props) => {
         </Link>
         <p>{props.ship.index}</p>
       <p>Capacity: {props.ship.capacity}</p>
-      <p>Berth: {props.ship.berth}</p>
+      <p>Berth: {berthstatus}</p>
       <p>Origin: {props.ship.origin}</p>
       <p>Destination: {props.ship.destination}</p>
       <p>Inspected: {inspected}</p>
